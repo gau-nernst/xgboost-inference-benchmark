e7f3682e27af66d1e652c38add05a74f95499872
Thien Tran <gau.nernst@yahoo.com.sg>
2022-10-10 11:21:23 +0800

    update readme and add results formatting

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # XGBoost Inference Benchmark
 
+[Benchmark results](https://github.com/gau-nernst/xgboost-inference-benchmark/issues/1)
+
 There are not many resources on deploying XGBoost models, especially optimizing for inference speed. This repo benchmarks inference speed for several libraries that claim to optimize inference speed for tree algorithms. If you know any other methods or libraries, do let me know and I will try to include it.
 
 Only CPU inference is supported.
@@ -18,15 +20,22 @@ Using conda
 ```bash
 conda create -n xgboost python=3.9
 conda activate xgboost
-conda install numpy
+conda install numpy scipy pandas tabulate
 
 pip install xgboost
-pip install onnxruntime git+https://github.com/onnx/onnxmltools.git
+conda install onnx onnxruntime -c conda-forge
+pip install git+https://github.com/onnx/onnxmltools.git
 pip install daal4py
 pip install treelite treelite-runtime
 ```
 
-Note: At the time of this writing, the latest release of ONNXMLTools does not support converting models from `xgboost>=1.6.1` without [this patch](https://github.com/onnx/onnxmltools/pull/567). Therefore, you have to install `onnxmltools` from the latest main branch.
+Note 1: At the time of this writing, the latest release of ONNXMLTools does not support converting models from `xgboost>=1.6.1` without [this patch](https://github.com/onnx/onnxmltools/pull/567). Therefore, you have to install `onnxmltools` from the latest main branch.
+
+Note 2: For Apple Silicon machines:
+
+- There are no pre-built `onnx` binaries from PyPI but there are from `conda-forge`.
+- For `treelite` and `treelite-runtime`, you might have to install OpenMP runtime beforehand i.e. `brew install libomp`.
+- `daal4py` is not supported, since it needs Intel CPU.
 
 ## Usage
 
--- benchmark.py
+++ benchmark.py
@@ -3,6 +3,7 @@ import time
 import daal4py as d4p
 import numpy as np
 import onnxruntime
+import pandas as pd
 import treelite_runtime
 import xgboost as xgb
 
@@ -10,19 +11,28 @@ import xgboost as xgb
 def compare(x1, x2):
     diff = np.abs(x1 - x2)
     rmse = (diff ** 2).mean() ** 0.5
-    max_diff = diff.max()
+    max_error = diff.max()
     print("RMSE:", rmse)
-    print("Max diff:", max_diff)
+    print("Max diff:", max_error)
+    return rmse, max_error
 
 
 def time_fn(fn, *args, **kwargs):
     N = 1000
+    fn(*args, **kwargs) # warm-up
     time0 = time.time()
     for _ in range(N):
         fn(*args, **kwargs)
-    avg_time = (time.time() - time0) / N
-    print(f"Time: {avg_time*1e3:.3f}ms")
-    print(f"Speed: {1/avg_time:.0f} it/s")
+    speed = N / (time.time() - time0)
+    print(f"Speed: {speed:.0f} it/s")
+    return speed
+
+
+d4p_version = ".".join(str(x) for x in eval(d4p._get__version__())[:-1])
+print("XGBoost:", xgb.__version__)
+print("ONNX Runtime:", onnxruntime.__version__)
+print("DAAL4PY:", d4p_version)
+print("Treelite Runtime:", treelite_runtime.__version__)
 
 
 n_samples, n_features = 10_000, 512
@@ -32,7 +42,7 @@ emb_1 = X[:1]
 emb_10 = X[:10]
 emb_100 = X[:100]
 
-n_threads = 10
+n_threads = 16
 bst = xgb.Booster()
 bst.load_model("xgboost_model.json")
 params = {
@@ -43,9 +53,9 @@ bst.set_param(params)
 
 print("XGBoost")
 probs = bst.inplace_predict(X)
-time_fn(bst.inplace_predict, emb_1)
-time_fn(bst.inplace_predict, emb_10)
-time_fn(bst.inplace_predict, emb_100)
+output_data = compare(probs, probs)
+speed_data = [time_fn(bst.inplace_predict, x) for x in [emb_1, emb_10, emb_100]]
+xgboost_data = [*output_data, *speed_data]
 print()
 
 
@@ -60,10 +70,9 @@ def onnx_pred(embs):
     return sess.run([onnx_out_name], {onnx_in_name: embs})[0][:, 1]
 
 onnx_probs = onnx_pred(X)
-compare(probs, onnx_probs)
-time_fn(onnx_pred, emb_1)
-time_fn(onnx_pred, emb_10)
-time_fn(onnx_pred, emb_100)
+output_data = compare(probs, onnx_probs)
+speed_data = [time_fn(onnx_pred, x) for x in [emb_1, emb_10, emb_100]]
+onnx_data = [*output_data, *speed_data]
 print()
 
 
@@ -78,10 +87,9 @@ def daal_pred(embs, algo):
     return algo.compute(embs, daal_model).probabilities[:, 1]
 
 daal_probs = daal_pred(X, get_daal_algo())
-compare(probs, daal_probs)
-time_fn(daal_pred, emb_1, get_daal_algo())
-time_fn(daal_pred, emb_10, get_daal_algo())
-time_fn(daal_pred, emb_100, get_daal_algo())
+output_data = compare(probs, daal_probs)
+speed_data = [time_fn(daal_pred, x, get_daal_algo()) for x in [emb_1, emb_10, emb_100]]
+daal_data = [*output_data, *speed_data]
 print()
 
 
@@ -91,8 +99,18 @@ def treelite_pred(embs):
     return predictor.predict(treelite_runtime.DMatrix(embs))
 
 treelite_probs = treelite_pred(X)
-compare(probs, treelite_probs)
-time_fn(treelite_pred, emb_1)
-time_fn(treelite_pred, emb_10)
-time_fn(treelite_pred, emb_100)
+output_data = compare(probs, treelite_probs)
+speed_data = [time_fn(treelite_pred, x) for x in [emb_1, emb_10, emb_100]]
+treelite_data = [*output_data, *speed_data]
 print()
+
+
+data = [xgboost_data, onnx_data, daal_data, treelite_data]
+names = ["XGBoost", "ONNX", "DAAL", "Treelite"]
+columns_1 = ["rmse", "max error"]
+columns_2 = [f"speed (batch {i})" for i in (1, 10, 100)]
+df = pd.DataFrame(data, index=names, columns=columns_1 + columns_2)
+df[columns_1] = df[columns_1].applymap("{:.2e}".format)
+df[columns_2] = df[columns_2].applymap("{:,.0f}".format)
+print(df)
+df.to_markdown("temp.txt", tablefmt="github")
